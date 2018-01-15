144133fa005cd8e98026b7f2138618419311a6bb
austincmatteson <austin.c.matteson@gmail.com>
2018-01-15 15:14:32 -0800

    creation of 1st & Pike object

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 'use strict';
 
+// Location	      Min / Cust	Max / Cust	Avg Cookie Sale
+// 1st and Pike	  23	  65	        6.3
+// SeaTac Airport	3	    24	        1.2
+// Seattle Center	11	  38	        3.7
+// Capitol Hill	  20	  38	        2.3
+// Alki	          2	    16	        4.6
+
+var firstAndPike = {
+  minHourlyCustomers: 23,
+  maxHourlyCustomers: 65,
+  avgCookiesPerSale: 6.3,
+}
